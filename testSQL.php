e48367c7a26310712fc46953acb499cb0cb79700
Kamakshi <kduvvuru@gmail.com>
2012-11-24 11:44:59 -0800

    second commit

--- testSQL.php
+++ testSQL.php
@@ -3,8 +3,6 @@ echo "<PRE>";
 
 include("config.php");
 
-echo "<PRE>";
-
 $fid = '622366350';
 $userInfo = mysql_query("SELECT * FROM allusers WHERE fid='$fid'");
 while($userRow = mysql_fetch_assoc($userInfo)){
@@ -18,6 +16,5 @@ while ($classesRow = mysql_fetch_assoc($classesInfo)) {
 //        $psetRow = mysql_fetch_assoc($psetTable);
 //        echo '<div class="draggable" id="'.$psetRow["pid"].'"><a href="question.php?pid='.$psetRow['pid'].'&qnum=1" data-role="button">'.$psetRow["class"]." ".$psetRow["pset"]."</a></div>";
 } 
-echo "<PRE>";
 
 ?>
